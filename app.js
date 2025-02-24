6395de2e5a9a7bfdfdf7e02ebe190f3be3e9d7d0
AryanEats <e22cseu1302@bennett.edu.in>
2025-02-24 22:37:37 +0530

    Fixes

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-import {puppeMaster} from './constants/exports.js';
+import { puppeMaster, linkedinBot } from './constants/exports.js';
 
-puppeMaster();
+const { browser, page } = await puppeMaster('windows');
+
+const runLinkedinBot = async () => {
+  try{
+    await linkedinBot(page);
+  } catch(error){
+    console.error('Error running Linkedin Bot:', error);
+  } finally{
+    await browser.close();
+  }
+}
+
+runLinkedinBot();
--- config/puppeMaster.js
+++ config/puppeMaster.js
@@ -20,26 +20,27 @@ const puppeMaster = async(setOs='windows') => {
         await page.setUserAgent(
             'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'
         );
-    }else if(setOs === 'macos'){
-        // await page.setUserAgent(
-        //     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
-        // );
-        console.log("Not supported for macos right now");
-    }else{
-        // await page.setUserAgent(
-        //     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
-        // );
-        console.log("Not supported for linux right now");
+    }
+    // else if(setOs === 'macos'){
+    //     // await page.setUserAgent(
+    //     //     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
+    //     // );
+    // }
+    else{
+        console.log("Not supported for other os right now");
+        await browser.close();
+        return;
     }
 
-    await linkedinBot(page);
+    return { browser, page };
 
-  } catch (error) {
-    console.error('An error occurred:', error);
+    // await linkedinBot(page);
 
-  } finally {
-    await browser.close();
-  }
+    }catch(error){
+        console.error('Some error occured:', error);
+        await browser.close();
+        throw error;
+    }
 }
 
 export default puppeMaster;
